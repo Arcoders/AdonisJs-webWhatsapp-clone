be9d50ae4cb415d0cf4d3c02e43f9c1574916ad6
arcoders <anonismaa@gmail.com>
2018-11-18 17:40:29 +0100

    authentication implemented

---
+++ server/app/Controllers/Http/AuthController.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const User = use('App/Models/User');
+
+class AuthController {
+
+    async register({ request }) {
+
+        const { email, password, username } = request.all();
+
+        await User.create({email, password, username})
+
+        return this.login(...arguments)
+
+    }
+
+    async login ({ request, auth }) {
+
+        const { email, password } = request.all()
+
+        const token = await auth.attempt(email, password)
+        
+        const user = await this.getUserBy(email);
+        
+        return { token, user }
+
+    }
+
+    async getUserBy(email) {
+
+        return await User.query().where('email', email).first()
+
+    }
+
+}
+
+module.exports = AuthController
